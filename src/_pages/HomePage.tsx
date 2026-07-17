7ebca0a83655d067489b5d15bf3ab3a90b9ddf4c
Rahim10020 <rahialighi@gmail.com>
2026-07-17 16:06:34 +0000

    update files

--- src/_pages/HomePage.tsx
+++ src/_pages/HomePage.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <div>
-      <section className="mx-auto max-w-6xl px-6 py-20"></section>
+      <section className="mx-auto max-w-6xl px-6 py-20">
+        <h1>Bonjour les boss</h1>
+      </section>
 
       <section className="border-t border-black/10"></section>
 
