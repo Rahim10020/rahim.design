e255722e894e82919d3f6ee689f2f2097f513743
Rahim10020 <rahialighi@gmail.com>
2026-07-20 16:26:27 +0000

    fix finnish

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainLayout from './_components/layout/MainLayout';
-import HomePage from './_pages/HomePage';
-import AboutPage from './_pages/AboutPage';
-import ContactPage from './_pages/ContactPage';
-import ProjectsList from './_pages/projects/index';
-import ProjectDetail from './_pages/projects/[slug]';
-import LearnList from './_pages/learn/index';
-import LearnArticle from './_pages/learn/[slug]';
-import { ROUTES } from './routes';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./_components/layout/MainLayout";
+import HomePage from "./_pages/HomePage";
+import AboutPage from "./_pages/AboutPage";
+import ContactPage from "./_pages/ContactPage";
+import ProjectsList from "./_pages/projects/index";
+import ProjectDetail from "./_pages/projects/[slug]";
+import LearnList from "./_pages/learn/index";
+import LearnArticle from "./_pages/learn/[slug]";
+import { ROUTES } from "./routes";
 
 const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: ROUTES.about, element: <AboutPage /> },
-      { path: ROUTES.contact, element: <ContactPage /> },
-      { path: ROUTES.projects.list, element: <ProjectsList /> },
-      { path: ROUTES.projects.detail, element: <ProjectDetail /> },
-      { path: ROUTES.learn.list, element: <LearnList /> },
-      { path: ROUTES.learn.article, element: <LearnArticle /> },
+      { path: ROUTES.ABOUT, element: <AboutPage /> },
+      { path: ROUTES.CONTACT, element: <ContactPage /> },
+      { path: ROUTES.PROJECTS.LIST, element: <ProjectsList /> },
+      { path: ROUTES.PROJECTS.DETAIL, element: <ProjectDetail /> },
+      { path: ROUTES.LEARN.LIST, element: <LearnList /> },
+      { path: ROUTES.LEARN.ARTICLE, element: <LearnArticle /> },
     ],
   },
 ]);
