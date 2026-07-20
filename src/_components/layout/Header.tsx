f02aba2b415b782943c561b7f539688ca6e55bfa
Rahim10020 <rahialighi@gmail.com>
2026-07-20 15:42:38 +0000

    coding header

--- src/_components/layout/Header.tsx
+++ src/_components/layout/Header.tsx
@@ -7,7 +7,27 @@
 // ];
 
 export default function Header() {
-  return <header className="">
-    
-  </header>;
+  return (
+    <header className="py-8 px-6 bg-red-500">
+      <div className="flex items-center justify-between">
+        <div>
+          <h2>Rahim</h2>
+        </div>
+        <nav className="flex items-center justify-between gap-8">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Learn</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </nav>
+      </div>
+    </header>
+  );
 }
