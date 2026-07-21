ec4a2f09ef3f19a3c067686454bedda0ad1560bd
Rahim10020 <rahialighi@gmail.com>
2026-07-21 12:11:11 +0000

    fix header

--- src/_components/layout/Header.tsx
+++ src/_components/layout/Header.tsx
@@ -3,43 +3,28 @@ import Logo from "../ui/Logo";
 import { ROUTES } from "../../routes";
 
 export default function Header() {
-  const leftNavLinks = [
+  const navLinks = [
     { to: ROUTES.ABOUT, label: "About" },
     { to: ROUTES.PROJECTS.LIST, label: "Projects" },
   ];
 
-  const rightNavLinks = [
-    { to: ROUTES.LEARN.LIST, label: "Learn" },
-    { to: ROUTES.CONTACT, label: "Contact" },
-  ];
+  // const rightNavLinks = [
+  //   { to: ROUTES.LEARN.LIST, label: "Learn" },
+  //   { to: ROUTES.CONTACT, label: "Contact" },
+  // ];
 
   return (
-    <header className="bg-foreground flex items-center">
-      <div className="flex items-center justify-between py-8 px-6">
-        <Logo size={32} />
-        <nav className="flex items-center justify-between gap-16">
-          {leftNavLinks.map((link) => (
-            <li key={link.to}>
-              <Link
-                to={link.to}
-                className="text-lg font-normal text-background"
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-          {rightNavLinks.map((link) => (
-            <li key={link.to}>
-              <Link
-                to={link.to}
-                className="text-lg font-normal text-background border-l border-background"
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </nav>
-      </div>
+    <header className="bg-foreground flex items-center justify-between py-8 px-6">
+      <Logo size={32} />
+      <nav className="flex items-center gap-8">
+        {navLinks.map((link) => (
+          <li key={link.to}>
+            <Link to={link.to} className="text-lg font-normal text-background">
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </nav>
     </header>
   );
 }
