e706da4fdd4c0a42287c79ab83097932395894c4
Rahim10020 <rahialighi@gmail.com>
2026-08-01 09:11:53 +0000

    cleaning ...

--- src/_components/layout/Header.tsx
+++ src/_components/layout/Header.tsx
@@ -1,41 +1,7 @@
-import { Link } from "react-router-dom";
-import Logo from "../ui/Logo";
-import { ROUTES } from "../../routes";
-
 export default function Header() {
-  const navLinks = [
-    { to: ROUTES.ABOUT, label: "About" },
-    { to: ROUTES.PROJECTS.LIST, label: "Projects" },
-  ];
-
-  // const rightNavLinks = [
-  //   { to: ROUTES.LEARN.LIST, label: "Learn" },
-  //   { to: ROUTES.CONTACT, label: "Contact" },
-  // ];
-
   return (
-    <header className="sticky ">
-      <div className="">
-        {/* Logo */}
-        <Logo size={32} />
-
-        {/* Navigation */}
-        <nav className="hidden lg:flex items-center gap-8">
-          {navLinks.map((link) => (
-            <li key={link.to}>
-              <Link
-                to={link.to}
-                className="text-lg font-normal text-background"
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </nav>
-
-        {/* Right */}
-        <div className=""></div>
-      </div>
+    <header className="sticky top-0 z-50 border-b border-foreground bg-background-alt text-foreground">
+      <div className=""></div>
     </header>
   );
 }
