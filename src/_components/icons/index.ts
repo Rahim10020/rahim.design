aa85cc3923f0f398ece8f59d3c8f4ddf7b999b60
Rahim10020 <rahialighi@gmail.com>
2026-08-22 22:13:34 +0000

    exports icons

---
+++ src/_components/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as AddPlusIcon } from "./AddPlusIcon";
+export { default as ChevronLeftIcon } from "./ChevronLeftIcon";
+export { default as ChevronRightIcon } from "./ChevronRightIcon";
+export { default as CloseIcon } from "./CloseLgIcon";
+export { default as HambugerIcon } from "./HamburgerMdIcon";
+export { default as InfoIcon } from "./InfoIcon";
