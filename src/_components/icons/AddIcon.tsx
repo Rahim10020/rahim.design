3b4a74fe93c5f9b2c89fc514c1aebf91ef721b69
Rahim10020 <rahialighi@gmail.com>
2026-08-22 22:45:09 +0000

    fixing add icon

--- src/_components/icons/AddIcon.tsx
+++ src/_components/icons/AddIcon.tsx
@@ -1,27 +1,37 @@
-
 import type { IconProps } from "./types";
 
 const AddPlusIcon = ({
   size = 24,
   color = "currentColor",
-  strokeWidth = 2,
   ...props
 }: IconProps) => (
   <svg
     width={size}
     height={size}
-    viewBox="0 0 24 24"
+    viewBox="0 0 14 14"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path
-      d="M6 12H12M12 12H18M12 12V18M12 12V6"
-      stroke={color}
-      strokeWidth={strokeWidth}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+    <g clip-path="url(#clip0_1545_11084)">
+      <path
+        d="M7 0.540039V13.54"
+        stroke={color}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M0.5 7H13.5"
+        stroke={color}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1545_11084">
+        <rect width="14" height="14" fill="white" />
+      </clipPath>
+    </defs>
   </svg>
 );
 
