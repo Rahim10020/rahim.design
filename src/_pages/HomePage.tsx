8fea45953744fb883299f0db3256e8bd646e8ad9
Rahim10020 <rahialighi@gmail.com>
2026-08-22 23:09:43 +0000

    layout

--- src/_pages/HomePage.tsx
+++ src/_pages/HomePage.tsx
@@ -2,16 +2,21 @@ export default function HomePage() {
   return (
     <div>
       {/* Hero section */}
-      <section className="mx-auto max-w-6xl px-6 py-20">
-        <p>Le premier paragraph</p>
-      </section>
-      
-      {/* Hero section */}
+      <section className="mx-auto max-w-6xl px-6 py-20"></section>
 
-      <section className="">
-        <p>Le deuxieme paragraph</p>
-      </section>
+      {/* About section */}
+      <section className=""></section>
 
+      {/* Services section */}
+      <section className=""></section>
+
+      {/* Projects section */}
+      <section className=""></section>
+
+      {/* Steps section */}
+      <section className=""></section>
+
+      {/* Contact section */}
       <section className=""></section>
     </div>
   );
