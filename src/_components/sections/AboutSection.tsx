e73e8e3a91f5a37fc5b58683d6025fcbfe654fa1
Rahim10020 <rahialighi@gmail.com>
2026-08-23 18:15:31 +0000

    fixing

--- src/_components/sections/AboutSection.tsx
+++ src/_components/sections/AboutSection.tsx
@@ -4,7 +4,7 @@ export default function AboutSection() {
   return (
     <section className="flex min-h-[calc(100svh-104px)] w-full bg-background">
       <div className="max-w-350 mx-auto flex w-full px-2">
-        <div className="mx-auto w-full max-w-6xl py-12 mt-16">
+        <div className="mx-auto w-full max-w-6xl py-12 mt-16 space-y-12">
           {/* First line */}
           <div className="flex items-start justify-between">
             <div className="border-r-2 pr-2">
@@ -17,27 +17,31 @@ export default function AboutSection() {
             </div>
           </div>
           {/* Second line */}
-          <div className="">
-            <h1 className="text-4xl md:text-6xl font-medium">Designer,</h1>
-            <h1 className="text-4xl md:text-6xl font-medium">Coder💀</h1>
+          <div className="space-y-2">
+            <h1 className="text-4xl md:text-7xl font-medium text-foreground">
+              Designer,
+            </h1>
+            <h1 className="text-4xl md:text-7xl font-medium text-foreground">
+              Coder💀
+            </h1>
           </div>
           {/* Third line */}
           <div className="flex justify-end">
-            <div className="border-l-2 pl-4 max-w-xl">
-              <p>
+            <div className="max-w-2xl space-y-4">
+              <p className="text-lg md:text-2xl font-normal text-foreground">
                 I'm the type of developer who notices when a button is
                 misaligned by 4 pixels.
               </p>
-              <p>
+              <p className="text-lg md:text-2xl font-normal text-foreground">
                 More seriously, I like to understand why an interface works —
                 and especially why it doesn't work.
               </p>
-              <p>
+              <p className="text-lg md:text-2xl font-normal text-foreground">
                 I work at the intersection of web design and development, with
                 an obsession with clean interfaces, details that matter, and
                 experiences that feel natural.
               </p>
-              <p>
+              <p className="text-lg md:text-2xl font-normal text-foreground">
                 My goal is simple: build products that you will be proud to show
                 off and that your users will enjoy using.
               </p>
