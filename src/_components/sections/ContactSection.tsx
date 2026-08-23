f880820b601a57bf98177eb0cd69e6cee4b26e63
Rahim10020 <rahialighi@gmail.com>
2026-08-23 22:04:22 +0000

    coding contact section

---
+++ src/_components/sections/ContactSection.tsx
@@ -0,0 +1,16 @@
+import { SECTION_IDS } from "../../routes";
+
+export default function ContactSection() {
+  return (
+    <section
+      id={SECTION_IDS.CONTACT}
+      className="flex min-h-[calc(100svh-104px)] w-full bg-background"
+    >
+      <div className="max-w-350 mx-auto flex w-full px-2">
+        <div className="mx-auto w-full max-w-6xl">
+            
+        </div>
+      </div>
+    </section>
+  );
+}
