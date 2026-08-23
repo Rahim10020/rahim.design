46bc71a9128aea50ff5c9d254f35d6e4ab2294d6
Rahim10020 <rahialighi@gmail.com>
2026-08-23 18:30:55 +0000

    fixes

--- src/_components/sections/AboutSection.tsx
+++ src/_components/sections/AboutSection.tsx
@@ -3,7 +3,7 @@ import Logo from "../ui/Logo";
 export default function AboutSection() {
   return (
     <section className="flex min-h-[calc(100svh-104px)] w-full bg-background">
-      <div className="max-w-350 mx-auto flex w-full px-2">
+      <div className="max-w-350 mx-auto flex w-full px-2 border-b-2 border-foreground">
         <div className="mx-auto w-full max-w-6xl py-12 mt-16 space-y-12">
           {/* First line */}
           <div className="flex items-start justify-between">
