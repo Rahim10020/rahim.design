03b9ed8402abff8e01cfbc1147d0a8ddc7442fbf
Rahim10020 <rahialighi@gmail.com>
2026-08-26 13:22:17 +0000

    coding detail header

--- src/_pages/projects/[slug].tsx
+++ src/_pages/projects/[slug].tsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from "react-router-dom";
 import { ROUTES } from "../../routes";
 import { getProjectBySlug } from "../../data/project";
+import { AddIcon } from "../../_components/icons";
 
 export default function ProjectDetail() {
   const { slug } = useParams();
@@ -23,7 +24,19 @@ export default function ProjectDetail() {
   return (
     <div className="mx-auto max-w-6xl px-6 py-20">
       {/* Detail header */}
-      <div></div>
+      <div className="flex items-center justify-between mb-8">
+        <a
+          href="#"
+          className="underline cursor-pointer text-foreground text-xl"
+        >
+          Close
+        </a>
+        {/* icons */}
+        <div className="flex items-center justify-between gap-6">
+          <AddIcon />
+          <AddIcon />
+        </div>
+      </div>
       {/* Content */}
       <div></div>
     </div>
