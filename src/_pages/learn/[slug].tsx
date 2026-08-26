d8777c2e202d900e4d2902baf6457ab7403d9c59
Rahim10020 <rahialighi@gmail.com>
2026-08-26 16:39:20 +0000

    fixes articles

--- src/_pages/learn/[slug].tsx
+++ src/_pages/learn/[slug].tsx
@@ -63,115 +63,106 @@ export default function LearnArticle() {
   }
 
   return (
-    <div className="mx-auto max-w-4xl px-6 py-20">
-      <div className="flex items-start gap-8">
-        {/* side image (for the book) no image for type "Notes" */}
-        <div className="pt-8">
+    <div className="mx-auto max-w-3xl px-6 py-20">
+      {/* Content */}
+      <div>
+        {/* All textual content will have "mx-auto max-w-xl mb-8" */}
+        <div className="mx-auto max-w-xl mb-8 mt-8 ">
+          <div className="flex items-center gap-4 mb-4">
+            <h1 className="text-foreground text-4xl md:text-6xl">
+              {article.title}
+            </h1>
+            <a
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <OpenLinkIcon />
+            </a>
+          </div>
+          <p className="text-foreground text-xl font-normal leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus
+            mattis aliquam. Phasellus ante arcu, semper quis mi a, volutpat
+            bibendum risus. Donec at quam pellentesque, aliquam ipsum sed
+          </p>
+        </div>
+        {/* All images will have "mx-auto max-w-4xl my-8" */}
+        <div className="mx-auto max-w-4xl my-8">
           <img
             src="/images/others/image_placeholder.svg"
             alt="placeholder image"
-            className="w-60 h-80 object-cover"
+            className="w-full h-100 object-cover"
           />
         </div>
-        {/* Content */}
-        <div>
-          {/* All textual content will have "mx-auto max-w-xl mb-8" */}
-          <div className="mx-auto max-w-xl mb-8 mt-8 ">
-            <div className="flex items-center gap-4 mb-4">
-              <h1 className="text-foreground text-4xl md:text-6xl">
-                {article.title}
-              </h1>
-              <a
-                href="http://"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="cursor-pointer"
-              >
-                <OpenLinkIcon />
-              </a>
-            </div>
-            <p className="text-foreground text-xl font-normal leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              tempus mattis aliquam. Phasellus ante arcu, semper quis mi a,
-              volutpat bibendum risus. Donec at quam pellentesque, aliquam ipsum
-              sed
-            </p>
-          </div>
-          {/* All images will have "mx-auto max-w-4xl my-8" */}
-          <div className="mx-auto max-w-4xl my-8">
-            <img
-              src="/images/others/image_placeholder.svg"
-              alt="placeholder image"
-              className="w-full h-100 object-cover"
-            />
-          </div>
-          <div className="mx-auto max-w-xl flex flex-col gap-4">
-            <p className="text-foreground text-xl font-normal leading-relaxed">
-              Pellentesque et metus ut est finibus fringilla. Curabitur sit amet
-              mi dolor. Nam fermentum mi in erat pellentesque venenatis. Mauris
-              justo ante, vulputate quis laoreet ac, mattis eget justo. Aliquam
-              euismod lobortis commodo. Nam et accumsan dui. Etiam sit amet
-              dolor quis elit tristique interdum non ac ex. Nullam massa lorem,
-              sodales viverra leo eget, blandit pharetra mauris. Integer sed
-              consequat libero. Aliquam venenatis semper tortor quis volutpat.
-              Vivamus sit amet molestie leo, eget luctus odio. Ut tempor nisl
-              sem, at congue urna ultricies eu. Maecenas scelerisque sagittis
-              lorem et auctor. Aenean et gravida enim.
-            </p>
-            <p className="text-foreground text-xl font-normal leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              tempus mattis aliquam. Phasellus ante arcu, semper quis mi a,
-              volutpat bibendum risus. Donec at quam pellentesque, aliquam ipsum
-              sed
-            </p>
-          </div>
-          <div className="mx-auto max-w-4xl my-8">
-            <img
-              src="/images/others/image_placeholder.svg"
-              alt="placeholder image"
-              className="w-full h-100 object-cover"
-            />
-          </div>
-          <div className="mx-auto max-w-xl flex flex-col gap-4">
-            <p className="text-foreground text-xl font-normal leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              tempus mattis aliquam. Phasellus ante arcu, semper quis mi a,
-              volutpat bibendum risus. Donec at quam pellentesque, aliquam ipsum
-              sed
-            </p>
-            <p className="text-foreground text-xl font-normal leading-relaxed">
-              Pellentesque et metus ut est finibus fringilla. Curabitur sit amet
-              mi dolor. Nam fermentum mi in erat pellentesque venenatis. Mauris
-              justo ante, vulputate quis laoreet ac, mattis eget justo. Aliquam
-              euismod lobortis commodo. Nam et accumsan dui. Etiam sit amet
-              dolor quis elit tristique interdum non ac ex. Nullam massa lorem,
-              sodales viverra leo eget, blandit pharetra mauris. Integer sed
-              consequat libero. Aliquam venenatis semper tortor quis volutpat.
-              Vivamus sit amet molestie leo, eget luctus odio. Ut tempor nisl
-              sem, at congue urna ultricies eu. Maecenas scelerisque sagittis
-              lorem et auctor. Aenean et gravida enim.
-            </p>
-          </div>
-          <div className="mx-auto max-w-xl flex flex-col gap-4 mt-4">
-            <p className="text-foreground text-xl font-normal leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              tempus mattis aliquam. Phasellus ante arcu, semper quis mi a,
-              volutpat bibendum risus. Donec at quam pellentesque, aliquam ipsum
-              sed
-            </p>
-            <p className="text-foreground text-xl font-normal leading-relaxed">
-              Pellentesque et metus ut est finibus fringilla. Curabitur sit amet
-              mi dolor. Nam fermentum mi in erat pellentesque venenatis. Mauris
-              justo ante, vulputate quis laoreet ac, mattis eget justo. Aliquam
-              euismod lobortis commodo. Nam et accumsan dui. Etiam sit amet
-              dolor quis elit tristique interdum non ac ex. Nullam massa lorem,
-              sodales viverra leo eget, blandit pharetra mauris. Integer sed
-              consequat libero. Aliquam venenatis semper tortor quis volutpat.
-              Vivamus sit amet molestie leo, eget luctus odio. Ut tempor nisl
-              sem, at congue urna ultricies eu. Maecenas scelerisque sagittis
-              lorem et auctor. Aenean et gravida enim.
-            </p>
-          </div>
+        <div className="mx-auto max-w-xl flex flex-col gap-4">
+          <p className="text-foreground text-xl font-normal leading-relaxed">
+            Pellentesque et metus ut est finibus fringilla. Curabitur sit amet
+            mi dolor. Nam fermentum mi in erat pellentesque venenatis. Mauris
+            justo ante, vulputate quis laoreet ac, mattis eget justo. Aliquam
+            euismod lobortis commodo. Nam et accumsan dui. Etiam sit amet dolor
+            quis elit tristique interdum non ac ex. Nullam massa lorem, sodales
+            viverra leo eget, blandit pharetra mauris. Integer sed consequat
+            libero. Aliquam venenatis semper tortor quis volutpat. Vivamus sit
+            amet molestie leo, eget luctus odio. Ut tempor nisl sem, at congue
+            urna ultricies eu. Maecenas scelerisque sagittis lorem et auctor.
+            Aenean et gravida enim.
+          </p>
+          <p className="text-foreground text-xl font-normal leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus
+            mattis aliquam. Phasellus ante arcu, semper quis mi a, volutpat
+            bibendum risus. Donec at quam pellentesque, aliquam ipsum sed
+          </p>
+        </div>
+        <div className="mx-auto max-w-4xl my-8">
+          <img
+            src="/images/others/image_placeholder.svg"
+            alt="placeholder image"
+            className="w-full h-100 object-cover"
+          />
+        </div>
+        <div className="mx-auto max-w-xl flex flex-col gap-4">
+          <p className="text-foreground text-xl font-normal leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus
+            mattis aliquam. Phasellus ante arcu, semper quis mi a, volutpat
+            bibendum risus. Donec at quam pellentesque, aliquam ipsum sed
+          </p>
+          <p className="text-foreground text-xl font-normal leading-relaxed">
+            Pellentesque et metus ut est finibus fringilla. Curabitur sit amet
+            mi dolor. Nam fermentum mi in erat pellentesque venenatis. Mauris
+            justo ante, vulputate quis laoreet ac, mattis eget justo. Aliquam
+            euismod lobortis commodo. Nam et accumsan dui. Etiam sit amet dolor
+            quis elit tristique interdum non ac ex. Nullam massa lorem, sodales
+            viverra leo eget, blandit pharetra mauris. Integer sed consequat
+            libero. Aliquam venenatis semper tortor quis volutpat. Vivamus sit
+            amet molestie leo, eget luctus odio. Ut tempor nisl sem, at congue
+            urna ultricies eu. Maecenas scelerisque sagittis lorem et auctor.
+            Aenean et gravida enim.
+          </p>
+        </div>
+        <div className="mx-auto max-w-xl flex flex-col gap-4 mt-4">
+          <p className="text-foreground text-xl font-normal leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus
+            mattis aliquam. Phasellus ante arcu, semper quis mi a, volutpat
+            bibendum risus. Donec at quam pellentesque, aliquam ipsum sed
+          </p>
+          <p className="text-foreground text-xl font-normal leading-relaxed">
+            Pellentesque et metus ut est finibus fringilla. Curabitur sit amet
+            mi dolor. Nam fermentum mi in erat pellentesque venenatis. Mauris
+            justo ante, vulputate quis laoreet ac, mattis eget justo. Aliquam
+            euismod lobortis commodo. Nam et accumsan dui. Etiam sit amet dolor
+            quis elit tristique interdum non ac ex. Nullam massa lorem, sodales
+            viverra leo eget, blandit pharetra mauris. Integer sed consequat
+            libero. Aliquam venenatis semper tortor quis volutpat. Vivamus sit
+            amet molestie leo, eget luctus odio. Ut tempor nisl sem, at congue
+            urna ultricies eu. Maecenas scelerisque sagittis lorem et auctor.
+            Aenean et gravida enim.
+          </p>
+        </div>
+        <div className="mx-auto max-w-xl mb-8 mt-8 ">
+          <a href="#" className="underline text-foreground text-xl">
+            Download and read it yourself
+          </a>
         </div>
       </div>
     </div>
