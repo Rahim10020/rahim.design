a3b2320a19c832abf6637c089c1b3e4b0b7e172a
Rahim10020 <rahialighi@gmail.com>
2026-08-27 14:04:38 +0000

    finish gsap ts file coding

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -7,3 +7,6 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 // enregistre les plugins une seule fois pour toute l'application
 gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollToPlugin);
+
+// exporte tout ce dont mes composants (les fichiers de mon projet) auront besoin
+export { gsap, useGSAP, ScrollTrigger, ScrollToPlugin };
