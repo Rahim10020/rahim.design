30ddef374ac3d830fe424202138f03404aa3c205
Rahim10020 <rahialighi@gmail.com>
2026-09-03 19:02:37 +0000

    coding about page

--- src/_pages/About.tsx
+++ src/_pages/About.tsx
@@ -5,12 +5,14 @@ export default function AboutPage() {
         <div className="mx-auto w-full max-w-6xl">
           <div className="flex items-center justify-between">
             <div>
-              <h1>About</h1>
-              <p>
+              <h1 className="text-4xl md:text-7xl font-medium text-foreground">
+                About
+              </h1>
+              <p className="text-foreground text-2xl leading-relaxed mx-auto max-w-xl">
                 I am a web developer, UX/UI designer and freelancer based at
                 Lome/Togo.
               </p>
-              <p>
+              <p className="text-foreground text-xl leading-relaxed mx-auto max-w-xl">
                 At first I just wanted to understand how things worked. Then I
                 started coding. Then I realized that writing code was only part
                 of the problem. We also had to understand what people were
@@ -21,7 +23,11 @@ export default function AboutPage() {
               </p>
             </div>
             <div>
-              <img src="/images/others/rahim-cartoon.png" alt="Rahim image" />
+              <img
+                src="/images/others/rahim-cartoon.png"
+                alt="Rahim image"
+                className="w-full h-full"
+              />
             </div>
           </div>
         </div>
