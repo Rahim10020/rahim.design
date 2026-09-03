3e18c94360e14b8b971b9ee8a9225a455dee0cc1
Rahim10020 <rahialighi@gmail.com>
2026-09-03 19:05:45 +0000

    fixing

--- src/_pages/About.tsx
+++ src/_pages/About.tsx
@@ -3,24 +3,26 @@ export default function AboutPage() {
     <section className="w-full bg-background min-h-screen">
       <div className="max-w-350 mx-auto px-6 pt-12 pb-24 mb-24">
         <div className="mx-auto w-full max-w-6xl">
-          <div className="flex items-center justify-between">
-            <div>
+          <div className="flex items-center justify-between gap-8">
+            <div className="flex flex-col gap-4">
               <h1 className="text-4xl md:text-7xl font-medium text-foreground">
                 About
               </h1>
-              <p className="text-foreground text-2xl leading-relaxed mx-auto max-w-xl">
-                I am a web developer, UX/UI designer and freelancer based at
-                Lome/Togo.
-              </p>
-              <p className="text-foreground text-xl leading-relaxed mx-auto max-w-xl">
-                At first I just wanted to understand how things worked. Then I
-                started coding. Then I realized that writing code was only part
-                of the problem. We also had to understand what people were
-                actually going to use. This is where design, UX and development
-                started to come together for me. Today I love being able to move
-                from idea to interface and then from interface to actual
-                product.
-              </p>
+              <div>
+                <p className="text-foreground text-2xl leading-relaxed mx-auto max-w-xl">
+                  I am a web developer, UX/UI designer and freelancer based at
+                  Lome/Togo.
+                </p>
+                <p className="text-foreground text-xl leading-relaxed mx-auto max-w-xl mt-6">
+                  At first I just wanted to understand how things worked. Then I
+                  started coding. Then I realized that writing code was only
+                  part of the problem. We also had to understand what people
+                  were actually going to use. This is where design, UX and
+                  development started to come together for me. Today I love
+                  being able to move from idea to interface and then from
+                  interface to actual product.
+                </p>
+              </div>
             </div>
             <div>
               <img
