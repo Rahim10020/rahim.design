b3333013a96a254752279051fcae35fce60cd5a7
Rahim10020 <rahialighi@gmail.com>
2026-09-03 20:21:31 +0000

    add knowme datas

---
+++ src/data/knowme.ts
@@ -0,0 +1,32 @@
+export interface KnowMe {
+  number: string;
+  title: string;
+  description: string;
+}
+
+export const knowmeData: KnowMe[] = [
+  {
+    number: "01",
+    title: "I ask questions",
+    description:
+      "Not to complicate the project. To understand what we are really trying to solve.",
+  },
+  {
+    number: "02",
+    title: "I pay attention to details",
+    description:
+      "A spacing, transition, empty state, or button may seem insignificant. Together they shape the experience.",
+  },
+  {
+    number: "03",
+    title: "I prefer clarity to complexity",
+    description:
+      "Si quelque chose peut être rendu plus simple sans perdre sa valeur, je chercherai généralement cette direction.",
+  },
+  {
+    number: "04",
+    title: "I speak to you frankly",
+    description:
+      "If I think an idea can be improved, I'll tell you. The goal is not just to produce what you ask for, but to build something that actually works.",
+  },
+];
