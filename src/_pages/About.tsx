759e9e4d0b04fb1c2b35f0191b536b49cc5320ba
Rahim10020 <rahialighi@gmail.com>
2026-09-03 18:58:45 +0000

    coding about page

--- src/_pages/About.tsx
+++ src/_pages/About.tsx
@@ -2,7 +2,29 @@ export default function AboutPage() {
   return (
     <section className="w-full bg-background min-h-screen">
       <div className="max-w-350 mx-auto px-6 pt-12 pb-24 mb-24">
-        <div className="mx-auto w-full max-w-6xl"></div>
+        <div className="mx-auto w-full max-w-6xl">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1>About</h1>
+              <p>
+                I am a web developer, UX/UI designer and freelancer based at
+                Lome/Togo.
+              </p>
+              <p>
+                At first I just wanted to understand how things worked. Then I
+                started coding. Then I realized that writing code was only part
+                of the problem. We also had to understand what people were
+                actually going to use. This is where design, UX and development
+                started to come together for me. Today I love being able to move
+                from idea to interface and then from interface to actual
+                product.
+              </p>
+            </div>
+            <div>
+              <img src="/images/others/rahim-cartoon.png" alt="Rahim image" />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
