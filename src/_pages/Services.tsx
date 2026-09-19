808c9796fbfc346d124106fd304e0e5c257ae926
Rahim10020 <rahialighi@gmail.com>
2026-09-19 09:57:23 +0000

    fixing services responsiveness

--- src/_pages/Services.tsx
+++ src/_pages/Services.tsx
@@ -48,25 +48,25 @@ export default function ServicesPage() {
           <div className="mb-24">
             <div className="flex flex-col gap-2 mb-32">
               <div className="flex items-center justify-end">
-                <h1 className="text-4xl font-medium text-foreground">
+                <h1 className="text-2xl lg:text-4xl font-medium text-foreground">
                   SERVICES
                 </h1>
               </div>
               <div>
-                <h1 className="text-4xl md:text-6xl font-medium text-foreground">
+                <h1 className="text-4xl md:text-6xl font-medium text-foreground max-w-xs md:max-w-md lg:max-w-none">
                   You have something to build.
                 </h1>
               </div>
             </div>
             {/* Center */}
             <div className="mx-auto max-w-3xl flex flex-col gap-8">
-              <p className="text-foreground text-2xl leading-relaxed max-w-2xl">
+              <p className="text-foreground text-2xl text-center md:text-left leading-relaxed max-w-sm md:max-w-2xl">
                 An idea, a product, a site or an experience that deserves to be
                 better thought out? I help you move from the idea to something
                 clear, beautiful and truly usable — from design to development.
               </p>
               {/* Buttons */}
-              <div className="flex items-center gap-4">
+              <div className="flex flex-col lg:flex-row items-center gap-4">
                 <Button className="px-6 py-4 text-xl font-medium bg-primary border-2 border-foreground text-foreground">
                   Let's talk about your project
                 </Button>
