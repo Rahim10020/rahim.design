36d2daf84fbf37900df78ce51d878f03d325d1bd
Rahim10020 <rahialighi@gmail.com>
2026-09-25 15:18:30 +0000

    Fix production blank page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import ProjectDetail from "./_pages/projects/[slug]";
 import LearnList from "./_pages/learn/index";
 import LearnArticle from "./_pages/learn/[slug]";
 import { ROUTES } from "./routes";
-import SEO from "./_components/SEO";
 
 const router = createBrowserRouter([
   {
@@ -26,12 +25,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return (
-    <>
-      <SEO />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/_components/layout/MainLayout.tsx
+++ src/_components/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import SEO from "../SEO";
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen flex-col">
+      <SEO />
       <Header />
       <main className="flex-1">
         <Outlet />
